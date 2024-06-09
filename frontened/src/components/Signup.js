b9684c76d4d2c5828e78d12f08beb697fb99e60c
Anshika Agarwal <anshikaagarwal0405@gmail.com>
2024-06-09 23:01:13 +0530

    connected react with node api

--- frontened/src/components/Signup.js
+++ frontened/src/components/Signup.js
@@ -1,23 +1,55 @@
-import React from 'react'
-import { useState } from 'react'
-
-
+import React, { useState } from 'react';
+import {useNavigate} from 'react-router-dom';
 function Signup() {
-const [name,setName]=useState("");
-const [password,setPasssword]=useState("");
-const [email,setEmail]=useState("");
-const collectData=()=>{
-    console.warn(name,email,password);
-}
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate=useNavigate();
+
+  const collectData=async ()=>{
+    console.log(name,email,password);
+    let result=await fetch('http://localhost:5000/register',{
+      method:'post',
+      body:JSON.stringify({name,email,password}),
+      headers:{
+        'Content-type':'application/json'
+      },
+    });
+    result=await result.json();
+    console.warn(result);
+    if(result){
+   navigate('/');
+    }
+  }
   return (
     <div className="register">
-      <h1>Resgister Here</h1>
-      <input  className="inputbox" type="text" placeholder="Enter Name"  value={name} onChange={(e)=>setName(e.target.value)}/>
-      <input className="inputbox" type="email" placeholder="Enter email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-      <input className="inputbox" type="password" placeholder="Enter Password" value={password} onChange={(e)=>setPasssword(e.target.value)}/>
+      <h1>Register Here</h1>
+      <input
+        className="inputbox"
+        type="text"
+        placeholder="Enter Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        className="inputbox"
+        type="email"
+        placeholder="Enter Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        className="inputbox"
+        type="password"
+        placeholder="Enter Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button type="button" onClick={collectData}>Sign Up</button>
+      {message && <p>{message}</p>}
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
