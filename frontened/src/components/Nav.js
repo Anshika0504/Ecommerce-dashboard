5ff4360eb4ccd05814fdf2703dfe0877f6669c53
Anshika Agarwal <anshikaagarwal0405@gmail.com>
2024-06-10 18:31:22 +0530

    completed with navbar

--- frontened/src/components/Nav.js
+++ frontened/src/components/Nav.js
@@ -1,35 +1,53 @@
-
-import { useEffect } from 'react';
-import {Link,useNavigate} from 'react-router-dom';
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 function Nav() {
-  const auth=localStorage.getItem('user');
-  const navigate=useNavigate();
-  useEffect(()=>{
-const auth=localStorage.getItem('user');
-if(auth){
-  navigate("/");
-}
-  },[])
-  const logout =()=>{
-  localStorage.clear();
-  navigate('/signup');
-  }
+  const auth = localStorage.getItem("user");
+  const navigate = useNavigate();
+  useEffect(() => {
+    const auth = localStorage.getItem("user");
+    if (auth) {
+      navigate("/");
+    }
+  }, []);
+  const logout = () => {
+    localStorage.clear();
+    navigate("/signup");
+  };
   return (
     <div>
-      <ul className="nav-ul">
-        <li><Link to="/">Products</Link></li>
-        <li><Link to="/add">Add Products</Link></li>
-        <li><Link to="/update">Upadate Products</Link></li>
-        <li><Link to="/profile">Profile</Link></li>
-        {
-          auth ? <li><Link onClick={logout} to="/signup">Logout</Link></li>:
-          <><li><Link to="/signup">Sign Up</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          </>
-        }
-      </ul>
+      <img className="logo" src="https://imgs.search.brave.com/TUFxL31an4Be3R3GKDAWPdKyiB-MuccaOx5xYXlJc3w/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/bG9nb2pveS5jb20v/d3AtY29udGVudC91/cGxvYWRzLzIwMTgv/MDUvMzAxNDM0MDkv/NV9iaWctNzY4eDU5/MS5wbmc" alt="logo"></img>
+      {auth ? (
+        <ul className="nav-ul">
+          <li>
+            <Link to="/">Products</Link>
+          </li>
+          <li>
+            <Link to="/add">Add Products</Link>
+          </li>
+          <li>
+            <Link to="/update">Upadate Products</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link onClick={logout} to="/signup">
+              Logout ({JSON.parse(auth).name})
+            </Link>
+          </li>
+        </ul>
+      ) : (
+        <ul className="nav-ul nav-right">
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      )}
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
